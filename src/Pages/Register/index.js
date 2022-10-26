c7acce6fb2f060b7d7934cbf02e526934b087664
rudy krisdiyanto <you@example.cocls>
2022-10-26 14:55:58 +0700

    Penempatan file

--- src/Pages/Register/index.js
+++ src/Pages/Register/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Auth from '../../Layout/Auth';
-import '../../Style/Pages/auth.css';
+import '../../Styles/Layout/Auth/auth.css';
 import { BsPerson } from 'react-icons/bs'
 import { CiLock } from 'react-icons/ci';
 import {BsEnvelope} from 'react-icons/bs';
