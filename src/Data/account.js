d128e8678cb66801ab43ee85d0acbd8b563cf59f
Erliana <erlianasrgt@gmail.com>
2022-10-26 13:07:40 +0700

    Update receiver card

--- src/Data/account.js
+++ src/Data/account.js
@@ -1,13 +1,18 @@
 import React from 'react'
 
 import Samuel from '../../src/Assets/account/samuelSuhi.svg'
+import Momo from '../../src/Assets/account/momoTaro.svg'
 
-export const Account = () => {
-    const listAccount = [
+const account = [
         {
             profilePic: Samuel,
             name: "Samuel Suhi",
             phone: "+62 813-8492-9994"
+        },
+        {
+            profilePic: Momo,
+            name: "Momo Taro",
+            phone: "+62 812-4343-6731"
         }
     ]
-}
+export default account
