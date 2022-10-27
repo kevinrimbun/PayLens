4c61eef39651d9fa14bd59ddab37ad8305900a50
Erliana <erlianasrgt@gmail.com>
2022-10-27 17:16:24 +0700

    first update

--- src/Data/account.js
+++ src/Data/account.js
@@ -5,11 +5,13 @@ import Momo from '../../src/Assets/account/momoTaro.svg'
 
 const account = [
         {
+            id: 1,
             profilePic: Samuel,
             name: "Samuel Suhi",
             phone: "+62 813-8492-9994"
         },
         {
+            id: 2,
             profilePic: Momo,
             name: "Momo Taro",
             phone: "+62 812-4343-6731"
---
+++ src/Pages/DetailTransfer/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Content from '../../Layout/Content'
+
+const DetailTransfer = () => {
+  return <Content>
+    
+  </Content>
+}
+
+export default DetailTransfer
