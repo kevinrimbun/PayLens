ee65d7489501a3d848b86591fd38afdb55b66de0
aldyfikhrimuhammad <aldyfikhrimuhammad@gmail.com>
2022-12-01 07:34:58 +0700

    Fix layout change pin

--- src/Pages/ChangePin/index.js
+++ src/Pages/ChangePin/index.js
@@ -1,8 +1,21 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+
+// Chakra UI
 import { PinInput, PinInputField } from '@chakra-ui/react';
+
+// Components
+import NavbarComp from "../../Components/Navbar";
+import Sidebar from "../../Components/Sidebar";
+import Footer from "../../Components/Footer";
+
+// Bootstrap
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+// CSS
 import '../../Styles/Pages/auth.css';
-import Content from '../../Layout/Content';
 import "../../Styles/Pages/ChangePin/changepin.css"
 
 const ChangePin = () => {
@@ -20,51 +33,66 @@ const ChangePin = () => {
         e.preventDefault();
         if (pin1.length === 0 && pin2.length === 0 && pin3.length === 0 && pin4.length === 0 && pin5.length === 0 && pin6.length === 0) {
             setError(true)
-        } else if ( pin1 !== localStorage.getItem("pin1") && pin2 !== localStorage.getItem("pin2") && pin3 !== localStorage.getItem("pin3") && pin4 !== localStorage.getItem("pin4") && pin5 !== localStorage.getItem("pin5") && pin6 !== localStorage.getItem("pin6")) {
+        } else if (pin1 !== localStorage.getItem("pin1") && pin2 !== localStorage.getItem("pin2") && pin3 !== localStorage.getItem("pin3") && pin4 !== localStorage.getItem("pin4") && pin5 !== localStorage.getItem("pin5") && pin6 !== localStorage.getItem("pin6")) {
             setError(true)
         } else {
-            navigate('/new-pin', {replace : true})
+            navigate('/new-pin', { replace: true })
         }
         const data = new FormData(e.target)
         console.log(Object.fromEntries(data.entries()));
     }
 
 
-    return <Content>
-        <div className='ChangePin-title'>
+    return <>
+        {/* Navbar Section */}
+        <NavbarComp />
 
-            <div className='title-top-right'>
-                Change Pin
-            </div>
-            <div className='subtitle-bottom-right'>
-            Enter your current 6 digits Zwallet PIN <br /> below to continue to the next steps.
-            </div>
+        <div className="App w-100 p-1">
+            <Container fluid className="w-100 p-5 Container-Section p-1">
+                <Row>
 
+                    {/* Sidebar Section */}
+                    <Col sm={3} className="Sidebar-Section p-1"><Sidebar /></Col>
+
+                    {/* Profile Section */}
+                    <Col sm={8} className="Profile-Section p-4 ms-3 shadow-lg d-flex flex-column justify-content-center align-items-center">
+                        <div className='ChangePin-title text-center'>
+                            <div className='title-top-right my-1'>
+                                Change Pin
+                            </div>
+                            <div className='subtitle-bottom-right my-0'>
+                                Enter your current 6 digits Paylens PIN <br /> below to continue to the next steps.
+                            </div>
+                        </div>
+
+                        <div className='ChangePin-wrapper'>
+                            <form onSubmit={handleSubmit}>
+                                <PinInput className='d-flex pin-input' size='sm' >
+                                    <PinInputField className='pin-input text-center' onChange={(e) => setPin1(e.target.value)} name='pin1' />
+                                    <PinInputField className='pin-input text-center' onChange={(e) => setPin2(e.target.value)} name='pin2' />
+                                    <PinInputField className='pin-input text-center' onChange={(e) => setPin3(e.target.value)} name='pin3' />
+                                    <PinInputField className='pin-input text-center' onChange={(e) => setPin4(e.target.value)} name='pin4' />
+                                    <PinInputField className='pin-input text-center' onChange={(e) => setPin5(e.target.value)} name='pin5' />
+                                    <PinInputField className='pin-input text-center' onChange={(e) => setPin6(e.target.value)} name='pin6' />
+                                </PinInput>
+                                <div className='error-message'>
+                                    {error && pin1.length === 0 && pin2.length === 0 && pin3.length === 0 && pin4.length === 0 && pin5.length === 0 && pin6.length === 0 ?
+                                        <label>Input cannot be empty !</label> : ""}
+                                </div>
+                                <div className='error-message'>
+                                    {error && pin1 !== localStorage.getItem("pin1") && pin2 !== localStorage.getItem("pin2") && pin3 !== localStorage.getItem("pin3") && pin4 !== localStorage.getItem("pin4") && pin5 !== localStorage.getItem("pin5") && pin6 !== localStorage.getItem("pin6") ?
+                                        <label>PIN Invalid !</label> : ""}
+                                </div>
+                                <button className="btn-auth" id='submit' type="submit" value="Enter">Continue</button>
+                            </form>
+                        </div>
+
+                    </Col>
+                </Row>
+            </Container>
         </div>
-
-        <div className='ChangePin-wrapper'>
-            <form onSubmit={handleSubmit}>
-                <PinInput className='d-flex pin-input' size='sm' >
-                    <PinInputField className='pin-input text-center' onChange={(e) => setPin1(e.target.value)} name='pin1'  />
-                    <PinInputField className='pin-input text-center' onChange={(e) => setPin2(e.target.value)} name='pin2'  />
-                    <PinInputField className='pin-input text-center' onChange={(e) => setPin3(e.target.value)} name='pin3'  />
-                    <PinInputField className='pin-input text-center' onChange={(e) => setPin4(e.target.value)} name='pin4'  />
-                    <PinInputField className='pin-input text-center' onChange={(e) => setPin5(e.target.value)} name='pin5'  />
-                    <PinInputField className='pin-input text-center' onChange={(e) => setPin6(e.target.value)} name='pin6'  />
-                </PinInput>
-                <div className='error-message'>
-                    {error && pin1.length === 0 && pin2.length === 0 && pin3.length === 0 && pin4.length === 0 && pin5.length === 0 && pin6.length === 0 ?
-                    <label>Input cannot be empty !</label>:""}
-                </div>
-                <div className='error-message'>
-                    {error && pin1 !== localStorage.getItem("pin1") && pin2 !== localStorage.getItem("pin2") && pin3 !== localStorage.getItem("pin3") && pin4 !== localStorage.getItem("pin4") && pin5 !== localStorage.getItem("pin5") && pin6 !== localStorage.getItem("pin6") ?
-                    <label>PIN Invalid !</label>:""}
-                </div>
-                <button className="btn-auth" id='submit' type="submit" value="Enter">Continue</button>
-            </form>
-        </div>
-
-  </Content>
+        <Footer />
+    </>
 }
 
 export default ChangePin
